24fa0c733c54fb607f01e741149350c6bc472be8
Yihui Xiong <yihui.xiong@hotmail.com>
2018-12-22 13:53:06 +0000

    add mqtt

--- main.cpp
+++ main.cpp
@@ -1,15 +1,45 @@
 #include <iostream>
 
-//#include "opencv2/core/core.hpp"
-//#include "opencv2/highgui/highgui.hpp"
-//#include "opencv2/imgproc.hpp"
+// #include "opencv2/core/core.hpp"
+// #include "opencv2/highgui/highgui.hpp"
+// #include "opencv2/imgproc.hpp"
 
 #include "opencv2/opencv.hpp"
 
 #include "Mtcnn.h"
 
+#include "stdlib.h"
+#include "string.h"
+#include "unistd.h"
+#include "MQTTClient.h"
+
+
+#define ADDRESS     "127.0.0.1"
+#define CLIENTID    "xyz"
+
 using namespace std;
 using namespace cv;
+
+
+void publish(MQTTClient client, char* topic, char* payload) {
+    MQTTClient_message pubmsg = MQTTClient_message_initializer;
+    pubmsg.payload = payload;
+    pubmsg.payloadlen = strlen((char *)(pubmsg.payload));
+    pubmsg.qos = 2;
+    pubmsg.retained = 0;
+    MQTTClient_deliveryToken token;
+    MQTTClient_publishMessage(client, topic, &pubmsg, &token);
+    MQTTClient_waitForCompletion(client, token, 1000L);
+    printf("Message '%s' with delivery token %d delivered\n", payload, token);
+}
+
+int on_message(void *context, char *topicName, int topicLen, MQTTClient_message *message) {
+    char* payload = (char*) (message->payload);
+    printf("Received operation %s\n", payload);
+    MQTTClient_freeMessage(&message);
+    MQTTClient_free(topicName);
+    return 1;
+}
 
 void PlotDetectionResult(Mat& frame, const std::vector<SMtcnnFace>& bbox)
 {
@@ -27,8 +57,32 @@ void PlotDetectionResult(Mat& frame, const std::vector<SMtcnnFace>& bbox)
     }
 }
 
+
+
 int main(int argc, char** argv)
 {
+    char msg_buf[64];
+    MQTTClient client;
+
+    MQTTClient_create(&client, ADDRESS, CLIENTID, MQTTCLIENT_PERSISTENCE_NONE, NULL);
+    MQTTClient_connectOptions conn_opts = MQTTClient_connectOptions_initializer;
+    // conn_opts.username = "";
+    // conn_opts.password = "";
+
+    MQTTClient_setCallbacks(client, NULL, NULL, on_message, NULL);
+
+    int rc;
+    if ((rc = MQTTClient_connect(client, &conn_opts)) != MQTTCLIENT_SUCCESS) {
+        printf("Failed to connect, return code %d\n", rc);
+        exit(-1);
+    }
+
+    //listen for operation
+    // MQTTClient_subscribe(client, "bus", 0);
+
+    // MQTTClient_subscribe(client, "bus", 0);
+    publish(client, "bus", "xyz");
+
     VideoCapture cap(0);
 
     if (!cap.isOpened())
@@ -44,6 +98,7 @@ int main(int argc, char** argv)
 
     double sumMs = 0;
     int count = 0;
+
 
     while (1)
     {
@@ -64,16 +119,22 @@ int main(int argc, char** argv)
         double t = 1000 * double(t2 - t1) / getTickFrequency();
         sumMs += t;
         ++count;
-        cout << "time = " << t << " ms, FPS = " << 1000 / t << ", Average time = " << sumMs / count <<endl;
-        cout << "faces: " << finalBbox.size() << endl;
+        cout << "faces = " << finalBbox.size() << ", time = " << t << " ms, FPS = " << 1000 / t << ", Average time = " << sumMs / count <<endl;
+
+        publish(client, "bus", "0");
+
         PlotDetectionResult(frame, finalBbox);
 
-	imwrite("/home/pi/view.jpg", frame);
-        //imshow("frame", frame);
+        // imshow("frame", frame);
+        imwrite("/home/pi/view.jpg", frame);
 
         if (waitKey(1) == 'q')
             break;
     }
 
+    MQTTClient_disconnect(client, 1000);
+    MQTTClient_destroy(&client);
+
     return 0;
 }
+
